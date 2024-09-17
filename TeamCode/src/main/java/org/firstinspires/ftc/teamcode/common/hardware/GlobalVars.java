51fa29fd444bf55ac958b6406a1d0817a7d66883
fl9ppy <adi.calinescu16@gmail.com>
2024-09-17 23:07:19 +0300

    .

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/GlobalVars.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.common.hardware;
+
+public class GlobalVars {
+
+    public enum Modedrive {
+        DRIVER_CONTROL,
+        TURBO,
+        PRECISION
+    }
+
+    public static final double TURBO_SCALE = 1.0, PRECISION_SCALE = 4.3, DRIVE_SCALE = 1.7;
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/globalVars.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.common.hardware;
-
-public class globalVars {
-
-}
